af10883118e55dc7330ccb72b5d8988546df9d5a
Dave Halter <davidhalter88@gmail.com>
2024-09-30 12:26:45 +0200

    Fine

--- test-data/packages/typedpkg/typedpkg/sample.py
+++ test-data/packages/typedpkg/typedpkg/sample.py
@@ -5,4 +5,5 @@ from typing import Iterable, Tuple
 # --zuban-diff     # type: (Iterable[str]) -> Tuple[str, ...]
 def ex(a: Iterable[str]) -> Tuple[str, ...]:
     """Example typed package."""
-    return list(a)
+# --zuban-diff     return list(a)
+    return ()
